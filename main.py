73e8c4098088e236e709e4dd23e5c715f69cb68e
Amit4218 <amitbhagat621@gmail.com>
2024-08-28 20:18:01 +0530

    beautiful_soup done

--- main.py
+++ main.py
@@ -1,127 +1,97 @@
-from bs4 import BeautifulSoup
-import requests
+from pymongo import MongoClient  # importing agent from client
+import pandas as pd  # importing pandas to write csv file
+from bs4 import BeautifulSoup  # for parsing and scraping the site
+import requests  # to get requests permission from the page
+import datetime  # to keep track of when the data was created
 
 
+link = requests.get("https://books.toscrape.com/catalogue/category/books_1/index.html")  # getting the html form the page
+soup = BeautifulSoup(link.text, "html.parser")  # parsing the page
 
-link = requests.get("https://books.toscrape.com/catalogue/category/books_1/index.html")
 
-soup = BeautifulSoup(link.text, "html.parser")
+# Function for creating a CSV file to store the data
+def dataToCsv(titles, images, ratings, instocks, prices):
+    # Creating a structure
+    Title_columns = {
+        "Book Names": titles,
+        "Book Prices": prices,
+        "Image Links": images,
+        "Availability": instocks,
+        "Book Rating": ratings
+    }
+
+    data = pd.DataFrame(Title_columns)
+    print(data)
+
+    # Save the data into a CSV file
+    data.to_csv("books_data.csv", index=False)
+    print("Data has been successfully written to 'books_data.csv'")
+
+
+# Function for sending the data to the database
+def database(titles, images, ratings, instocks, prices):
+    client = MongoClient("connection string")
+
+    db = client.beautiful_soup_test
+
+    for title, image, rating, instock, price in zip(titles, images, ratings, instocks, prices):
+        post = {
+            "Book name": title,
+            "Image link": image,
+            "Rating": rating,
+            "Price": price,
+            "Availability": instock,
+            "date": datetime.datetime.now(tz=datetime.timezone.utc),
+        }
+
+        posts = db.posts
+        post_id = posts.insert_one(post).inserted_id
+
+    print("Data sending complete")
+    return post_id
 
 
 def main():
+    # Fetching elements required for scraping
+    name_elements = soup.find_all("h3")
+    rating_elements = soup.find_all('article', class_="product_pod")
+    image_elements = soup.find_all('div', class_="image_container")
+    price_elements = soup.find_all('div', class_="product_price")
 
-    # fetching elements which is required for scraping
+    # Lists to store the scraped data
+    titles = []
+    images = []
+    prices = []
+    instocks = []
+    ratings = []
 
-    name_element = soup.find_all("h3")   
+    # Looping through the elements
+    for book in name_elements:
+        title = book.find('a')['title']
+        titles.append(title)
 
-    rating_elements = soup.find_all('article', class_="product_pod")    
+    for image in image_elements:
+        img_src = image.find('img')['src']
+        full_img_url = "https://books.toscrape.com/" + img_src.replace("../../../", "")
+        images.append(full_img_url)
 
-    image_elements = soup.find_all('div', class_="image_container")    
+    for price in price_elements:
+        price_text = price.find('p').text.strip()
+        prices.append(price_text)
 
-    price_element = soup.find_all('div', class_="product_price")      
-  
+    for stock in rating_elements:
+        instock_text = stock.find('p', class_="instock availability").text.strip()
+        instocks.append(instock_text)
 
+    for rating in rating_elements:
+        rating_class = rating.find('p')['class']
+        # Assuming the rating class format is like 'star-rating Three'
+        ratings.append(rating_class[1])
 
-    # looping through the elements 
-
-    for elements in name_element, rating_elements, image_elements, price_element:
-
-            title = elements.find('a')['title']  # Find the 'a' tag and get the 'title' attribute
-
-            print(title)
-
-            image = elements.find('img')['src']
-            image = image.replace("../../../","")
-
-            print(image)
-
-            price = elements.find('p').text
-
-            instock = elements.find('p', class_="instock availability").text
-            instock = instock.replace(" ","")
-
-            print(instock)
-
-            rating = elements.find('p')['class']
-
-            print(rating)
+    # Calling functions with the collected data
+    dataToCsv(titles, images, ratings, instocks, prices)
+    database(titles, images, ratings, instocks, prices)
 
 
+# Run the main function to start scraping and storing data
 main()
-
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# elements = soup.find_all('article', class_="product_pod")
-
-# for element in elements:
-
-#     title = elements.find('a')['title']  # Find the 'a' tag and get the 'title' attribute
-#     print(title)
-
-#     image = elements.find('img')['src']
-#     image = image.replace("../../../","")
-#     print(image)
-
-
-
-
-# name_element = soup.find_all("h3")
-
-# for book_name in name_element:
-#     title = book_name.find('a')['title']  # Find the 'a' tag and get the 'title' attribute
-#     print(title)
-
-
-# image_elements = soup.find_all('div', class_="image_container")
-
-# for image_url in image_elements:
-
-#     image = image_url.find('img')['src']
-#     image = image.replace("../../../","")
-#     # print(image)
-
-
-# price_element = soup.find_all('div', class_="product_price")
-
-# for prices in price_element:
-#     price = prices.find('p').text
-#     # print(price)
-
-#     instock = prices.find('p', class_="instock availability").text
-#     instock = instock.replace(" ","")
-#     print(instock)
-
-# rating_elements = soup.find_all('article', class_="product_pod")
-
-# for ratings in rating_elements:
-#     rating = ratings.find('p')['class']
-
-#     print(rating)
